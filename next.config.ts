66e422ab6014f44e15a5d0c56c1ae8c4b6a50b59
Jefferson Reyes <jeffer@JEFF.localdomain>
2026-01-29 23:42:19 -0500

    Arreglar exportacion para S3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // <--- ESTA ES LA CLAVE
 };
 
 export default nextConfig;
